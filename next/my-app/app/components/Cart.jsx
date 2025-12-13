d6c1eb24697e928c219c364c724adfffae8d884d
harshvardhansharma2105 <harshvardhan.2105sharma@gmail.com>
2025-12-13 08:19:54 +0000

    updated cart.js

--- next/my-app/app/components/Cart.jsx
+++ next/my-app/app/components/Cart.jsx
@@ -1,14 +1,16 @@
 "use client"
 
 import {  useSelector } from "react-redux";
+import Link from "next/link";
 
 export default function Cart(){
-    const count = useSelector((state)=>{state.count.value});
+    const count = useSelector((state)=>state.count.value);
 
     return(
         <>
-            <img src="hello"/>
-            <div>items : {count}</div>
+            <Link href='/go-to-cart'>
+                <span><img src="https://d2ati23fc66y9j.cloudfront.net/ubuy/header-img/cart.svg"/>{count}</span>
+            </Link>
         </>
     );
 }
