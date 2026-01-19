745a5030a114ee1e56dd9ffcf0aca91b4f900130
harshvardhansharma21 <harshvardhan.sharma@ubuy.com>
2026-01-19 17:02:14 +0530

    updated file

--- login-signup/my-next-app/app/login/page.tsx
+++ login-signup/my-next-app/app/login/page.tsx
@@ -1,12 +1,10 @@
 import Login from '../Components/Login';
-import ProductCard from '../Components/ProductCard';
 export default function login(){
     return(
         <>
             <h1>Login Form</h1>
             <Login/>
 
-            <ProductCard minimal={true} />
         </>
     );
 }
